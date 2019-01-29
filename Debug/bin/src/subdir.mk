b53af6e612e441f5c882e74925e93053e511bac1
Daniel Manz <manzdani@hu-berlin.de>
2019-01-29 11:37:24 +0100

    cleaned up a little

--- Debug/bin/src/subdir.mk
+++ Debug/bin/src/subdir.mk
@@ -3,6 +3,18 @@
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
+O_SRCS += \
+../bin/src/ReadFCIDUMP.o \
+../bin/src/diagonalizer.o \
+../bin/src/main.o \
+../bin/src/rhf.o \
+../bin/src/rhf_equations.o \
+../bin/src/test_ReadFCIDUMP.o \
+../bin/src/test_diagonalizer.o \
+../bin/src/test_rhf.o \
+../bin/src/test_rhf_equations.o \
+../bin/src/testhelpers.o 
+
 
 # Each subdirectory must supply rules for building sources it contributes
 
